8b34dda58cc94196d6174fc5b7dba4ae9d0f2773
Craig Houston <craighouston01@gmail.com>
2022-12-21 12:14:26 +0000

    Start to build out homepage using Chakra UI
    Adds Navbar, SearchBar and SubmissionCard components to start to build
    out the home page. All build using Chakra components.
    
    Currently displays a Navbar with search and a few dummy
    buttons plus a stack of 2 identical Submissions displaying the
    subreddit name, author name, title, image and a few buttons.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css';
 import {useQuery, QueryClientProvider, QueryClient} from "@tanstack/react-query"
 import React from 'react';
 import axios from 'axios';
+import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Icon, IconProps, Image, Input, InputGroup, InputLeftElement, OmitCommonProps, Spacer, Stack, Text } from '@chakra-ui/react'
+import { ChatIcon, DownloadIcon, ExternalLinkIcon, HamburgerIcon, SearchIcon,} from '@chakra-ui/icons';
+
 
 const queryClient = new QueryClient();
 
@@ -13,26 +15,107 @@ const fetcher = () =>
   export const App: React.FunctionComponent = () => {
     return (
       <QueryClientProvider client={queryClient}>
-        <Submission></Submission>
+        <Navbar />
+        <Stack spacing='16' p='16' bg="brand.back">
+          <SubmissionCard/>
+          <SubmissionCard />
+        </Stack>
       </QueryClientProvider>
     );
   }
+
+const CircleIcon = (props: JSX.IntrinsicAttributes & OmitCommonProps<React.SVGProps<SVGSVGElement>, keyof IconProps> & IconProps & { as?: "svg" | undefined; }) => (
+  <Icon viewBox='0 0 200 200' {...props}>
+    <path
+      fill='orange'
+      d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+    />
+  </Icon>
+)
+
+function SearchBar() {
+  return (
+    <Stack spacing={4}>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents='none'
+          children={<SearchIcon color='gray.300' />}
+        />
+        <Input placeholder='Search Reddit-ish' />
+      </InputGroup>
+    </Stack>
+  )
+}
+
+function Navbar() {
+  return (
+    <Box position="static" top={0} p={4} zIndex={1}>
+      <Flex gap='2'>
+        <Box p='2'>
+          <Heading size='md' position={'static'}>reddit-ish</Heading>
+        </Box>
+        <Spacer />
+        <SearchBar />
+        <Spacer />
+        <ButtonGroup gap='2'>
+          <Button>Get App</Button>
+          <Button colorScheme='orange'>Log in</Button>
+          <Button><HamburgerIcon /></Button>
+        </ButtonGroup>
+      </Flex>
+    </Box>
+  );
+};
 
 function useSubmission() {
   return useQuery(["sample"], fetcher);
 }
 
-function Submission() {
+function SubmissionCard() {
   const { data, error, isLoading } = useSubmission();
 
   if (isLoading) return <p>Loading</p>;
   if (error) return <p>An error occurred</p>;
 
   return (
-    <div>
-      <h5>Posted by {data.author}</h5>
-      <h3>{data.title}</h3>
-      <a href={data.url}>Link</a>
-    </div>
+    <Card maxW='md' bg="white">
+      <CardHeader>
+        <Flex>
+          <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+            <CircleIcon boxSize={8} color='red.500' />
+
+            <Box>
+              <Heading size='sm'>{data.subreddit_name_prefixed}</Heading>
+              <Text>Posted by {data.author}</Text>
+            </Box>
+          </Flex>
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <Text>
+          {data.title}
+        </Text>
+      </CardBody>
+      <Image
+        objectFit='cover'
+        src='https://picsum.photos/200/100'
+        alt='Chakra UI'
+      />
+
+      <CardFooter
+        justify='space-between'
+        flexWrap='wrap'
+        sx={{
+          '& > button': {
+            minW: '136px',
+          },
+        }}
+      >
+        <ChatIcon /> 88 Comments
+        <ExternalLinkIcon /> Share
+        <DownloadIcon /> Save
+        <HamburgerIcon />
+      </CardFooter>
+    </Card>
   )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './App';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const colors = {
+  brand: {
+    back: '#DCEBEE'
+  },
+}
+
+const theme = extendTheme({ colors })
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App/>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
